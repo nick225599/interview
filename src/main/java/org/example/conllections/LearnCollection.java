9fa03083b02f2d1ae6624fd74c2789d8278fb93d
nick225599 <nick225599@163.com>
2022-06-22 23:59:51 +0800

    ~

---
+++ src/main/java/org/example/conllections/LearnCollection.java
@@ -0,0 +1,10 @@
+package org.example.conllections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class LearnCollection {
+    public static void main(String[] args) {
+        Collection<?> collection = new ArrayList<>();
+    }
+}
